b6d3139d2f12090e6444689ed5c714a775f819e8
sandratriveno <sandra.triveno@gmai.com>
2025-02-11 08:21:11 -0500

    initial project2

---
+++ src/main/java/pe/edu/idat/app_cloud_client/AppCloudClientApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_cloud_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCloudClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCloudClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/app_cloud_client/AppCloudClientApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_cloud_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCloudClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
