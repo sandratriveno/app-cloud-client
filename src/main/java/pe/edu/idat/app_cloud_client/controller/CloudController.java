c74e256c3f9bbaf55a93bc9dc5c711b3dd7c60f4
sandratriveno <sandra.triveno@gmai.com>
2025-02-11 09:31:06 -0500

    ejercicio terminado

---
+++ src/main/java/pe/edu/idat/app_cloud_client/controller/CloudController.java
@@ -0,0 +1,19 @@
+package pe.edu.idat.app_cloud_client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class CloudController {
+
+    @Value("${mensaje:Default mensaje}")
+    private String mensaje;
+    @GetMapping("/mensaje")
+    public String verMensajeCloud(){
+        return mensaje;
+
+
+    }
+}
